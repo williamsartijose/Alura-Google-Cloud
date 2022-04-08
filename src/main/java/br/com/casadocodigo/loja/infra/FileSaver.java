ce6b51ca939a4cdba673d7ca7fc4a0635b33c07b
williamsartijose <williamsartijose@hotmail.com>
2022-04-08 11:24:17 -0300

    Alterando classe FileSaver

--- src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
+++ src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
@@ -1,42 +1,35 @@
 package br.com.casadocodigo.loja.infra;
 
-import java.io.File;
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import br.com.casadocodigo.loja.models.Produto;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.Storage.BlobTargetOption;
+import com.google.cloud.storage.Storage.PredefinedAcl;
+import com.google.cloud.storage.StorageOptions;
 
 @Component
-public class FileSaver {
+public class FileSaver
+{
 
-	@Autowired
-	private HttpServletRequest request;
+    private static Storage storage = StorageOptions.getDefaultInstance().getService();
 
-	public String write(MultipartFile file) {
-		try {
-			File arquivo = new File(request.getServletContext().getRealPath(
-					"/resources/imagens"), file.getOriginalFilename());
-			file.transferTo(arquivo);
-			
-			return file.getOriginalFilename();
-			
-		} catch (IllegalStateException | IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
+    public String write(
+        MultipartFile file )
+    {
+        try {
+            final BlobInfo blobInfo = storage.create( BlobInfo.newBuilder( "casadocodigo-williamsartijose",
+                file.getOriginalFilename() ).build(),
+                file.getBytes(),
+                BlobTargetOption.predefinedAcl( PredefinedAcl.PUBLIC_READ ) );
+            return blobInfo.getMediaLink();
+
+        } catch( IllegalStateException | IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
 }
-
-
-
-
-
-
-
-
-
